0649cff06fc77ee427ea798150bb957c1f13d76c
LinjianZhang <zlj@terranovatelecom.com>
2018-01-17 18:00:44 +0800

    prism app learning

---
+++ PrismApp/Models/TestModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace PrismApp.Models
+{
+    class TestModel: BindableBase
+    {
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set { SetProperty(ref message, value); }
+        }
+    }
+}
---
+++ PrismApp/ViewModels/TestViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrismApp.Models;
+using Prism.Mvvm;
+
+namespace PrismApp.ViewModels
+{
+    class TestViewModel:BindableBase
+    {
+        private TestModel testModel;
+        public TestViewModel()
+        {
+            testModel = new TestModel();
+            testModel.Message = "this is prism example";
+        }
+
+        public TestModel TestModel
+        {
+            get
+            {
+                return testModel;
+            }
+            set
+            {
+                SetProperty(ref testModel, value);
+            }
+        }
+    }
+}
